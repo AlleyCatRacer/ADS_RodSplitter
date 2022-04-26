3d9ddfc21dda502066eb71e79614a28c3f1e4ea9
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2022-04-26 12:12:13 +0200

    Finished Rod + RodTest

--- src/Rod.java
+++ src/Rod.java
@@ -1,23 +1,76 @@
+import java.util.HashMap;
+
 public class Rod
 {
-	private static Integer[] prices = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
-	private static Integer[] best = {0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,};
+	private static final HashMap<Integer, Integer> bestPrice = new HashMap<>();
+	private static final int[] prices = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+	// Length:	0	1	2	3	5	6	7	8	9	10
+	// Price:	0	1	5	8	9	10	17	17	20	24
 	
-	public static int cut(int size)
+	public int bruteForceCut(int size)
 	{
 		int max = prices[size];
-		for (int i = 0; i < size; i++)
+		
+		if (size == 1)
 		{
-			max = Math.max(max, cut(size-i) + prices[i]);
+			return prices[1];
+		}
+		
+		for (int i = 1; i < size; i++)
+		{
+			// Too smart to be first step max = Math.max(max, bruteForceCut(size-i) + prices[i]);
+			int value = bruteForceCut(size - i);
+			if (prices[i] + value > max)
+			{
+				max = prices[i] + value;
+			}
 		}
 		return max;
 	}
 	
-	public static int dynamicCut(int size)
+	public int recursiveCut(int size)
 	{
-		for (int i = 1; i < size; i++)
+		System.out.println("I went recursive.");
+		if (size <= 1)
 		{
-		
+			return prices[size];
 		}
+		
+		int max = prices[size];
+		
+		for (int i = 2; i < prices.length + 1 && i < size; i++)
+		{
+			int value;
+			
+			if (bestPrice.get(size - i) == null)
+			{
+				value = recursiveCut(size - i);
+				bestPrice.put(size - i, value);
+			}
+			else
+			{
+				value = bestPrice.get(size - i);
+			}
+			if (prices[i] + value > max)
+			{
+				max = prices[i] + value;
+			}
+		}
+		
+		return max;
+	}
+	
+	public int dynamicCut(int size)
+	{
+		if (bestPrice.containsKey(size))
+		{
+			return bestPrice.get(size);
+		}
+		
+		int max = recursiveCut(size);
+		
+		bestPrice.put(size, max);
+		
+		return max;
 	}
 }
--- src/RodTest.java
+++ src/RodTest.java
@@ -1,6 +1,94 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RodTest
 {
-	@Test void
+	private Rod rod;
+	
+	@BeforeEach void setUp()
+	{
+		rod = new Rod();
+	}
+	
+	@Test void bruteForceTest()
+	{
+		assertEquals(0, rod.bruteForceCut(0));
+		assertEquals(1, rod.bruteForceCut(1));
+		assertEquals(5, rod.bruteForceCut(2));
+		assertEquals(8, rod.bruteForceCut(3));
+		assertEquals(10, rod.bruteForceCut(4));
+		assertEquals(13, rod.bruteForceCut(5));
+		assertEquals(17, rod.bruteForceCut(6));
+		assertEquals(18, rod.bruteForceCut(7));
+		assertEquals(22, rod.bruteForceCut(8));
+		assertEquals(25, rod.bruteForceCut(9));
+		assertEquals(30, rod.bruteForceCut(10));
+	}
+	
+	@Test void recursiveCutTest()
+	{
+		assertEquals(0, rod.recursiveCut(0));
+		assertEquals(1, rod.recursiveCut(1));
+		assertEquals(5, rod.recursiveCut(2));
+		assertEquals(8, rod.recursiveCut(3));
+		assertEquals(10, rod.recursiveCut(4));
+		assertEquals(13, rod.recursiveCut(5));
+		assertEquals(17, rod.recursiveCut(6));
+		assertEquals(18, rod.recursiveCut(7));
+		assertEquals(22, rod.recursiveCut(8));
+		assertEquals(25, rod.recursiveCut(9));
+		assertEquals(30, rod.recursiveCut(10));
+	}
+	
+	@Test void dynamicCutTestWithoutMapping()
+	{
+		// Each should print once
+		assertEquals(0, rod.dynamicCut(0));
+		System.out.println("---------------------------------------");
+		assertEquals(1, rod.dynamicCut(1));
+		System.out.println("---------------------------------------");
+		assertEquals(5, rod.dynamicCut(2));
+		System.out.println("---------------------------------------");
+		assertEquals(8, rod.dynamicCut(3));
+		System.out.println("---------------------------------------");
+		assertEquals(10, rod.dynamicCut(4));
+		System.out.println("---------------------------------------");
+		assertEquals(13, rod.dynamicCut(5));
+		System.out.println("---------------------------------------");
+		assertEquals(17, rod.dynamicCut(6));
+		System.out.println("---------------------------------------");
+		assertEquals(18, rod.dynamicCut(7));
+		System.out.println("---------------------------------------");
+		assertEquals(22, rod.dynamicCut(8));
+		System.out.println("---------------------------------------");
+		assertEquals(25, rod.dynamicCut(9));
+		System.out.println("---------------------------------------");
+		assertEquals(30, rod.dynamicCut(10));
+		System.out.println("---------------------------------------");
+	}
+	
+	@Test void dynamicCutTestWithPartialMapping3None1None6()
+	{
+		// Prints 3 times
+		rod.dynamicCut(3);
+		System.out.println("---------------------------------------");
+		
+		// Shouldn't print
+		assertEquals(8, rod.dynamicCut(3));
+		System.out.println("---------------------------------------");
+		
+		// Should print once because 3 is the highest it has mapped, so it needs one more recursive call
+		assertEquals(10, rod.dynamicCut(4));
+		System.out.println("---------------------------------------");
+		
+		// Shouldn't print
+		assertEquals(5, rod.dynamicCut(2));
+		System.out.println("---------------------------------------");
+		
+		// Should print 6 times because 10 - 6 = 4
+		assertEquals(30, rod.dynamicCut(10));
+		System.out.println("---------------------------------------");
+	}
 }
